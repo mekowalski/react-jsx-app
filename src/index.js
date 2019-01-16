41f20ac2276524b96a2e710f55084f9e1082880d
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-16 13:49:43 -0700

    refactor and refernce js variables

--- src/index.js
+++ src/index.js
@@ -6,11 +6,17 @@ import ReactDOM from 'react-dom';
 
 //Create a React Component(ES6)
 const App = () => {
+  const buttonText = 'Click Me'
+
   return (
     <div>
-      <label className="label" htmlFor='name'>Enter Name: </label>
+      <label className="label" htmlFor='name'>
+        Enter Name:
+      </label>
       <input id="name" type="text"/>
-      <button style={{ backgroundColor: 'green', color: 'white' }}>Submit</button>
+      <button style={{ backgroundColor: 'green', color: 'white' }}>
+        {buttonText}
+      </button>
     </div>
   )
 };
