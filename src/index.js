a1614d04dcdf7ce971ee1e2e72913744910594b2
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-16 13:45:22 -0700

    change syntax for label

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import ReactDOM from 'react-dom';
 const App = () => {
   return (
     <div>
-      <label class="label" for='name'>Enter Name: </label>
+      <label className="label" htmlFor='name'>Enter Name: </label>
       <input id="name" type="text"/>
       <button style={{ backgroundColor: 'green', color: 'white' }}>Submit</button>
     </div>
