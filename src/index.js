8bc3b3e5e5e8ed931a6390b361e9db888db88535
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-16 11:27:29 -0700

    move and rename jsx-app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//want to figure out how to use React to get content on the screen
+//Import the React and ReactDOM libraries
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+//Create a React Component
+//testing new remote
+
+
+//Take React Component and shoe it on the screen
