6d48e0e321462e7e3a314f0b81a4245ffb2e3a37
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-16 11:47:50 -0700

    finish step 2 and 3

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 //want to figure out how to use React to get content on the screen
 //Import the React and ReactDOM libraries
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 
 //Create a React Component
-//testing new remote
+const App = () => {
+  return <div>Hi There!</div>;
+};
 
 
 //Take React Component and shoe it on the screen
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root') //reference public/index.html, built in native function
+);
